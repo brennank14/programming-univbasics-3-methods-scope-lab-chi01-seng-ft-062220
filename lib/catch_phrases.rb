d75a4cb24ac2414372ba45cdf00e82e5ee7ac6c3
Kathleen Brennan <brennank14@gmail.com>
2020-06-04 22:31:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,16 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  help
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases(mario, toadstool, link)
+  puts phrase
+  puts status
+  puts help
+end
+
+all_phrases
